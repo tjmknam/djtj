ddf12707a644af7fe733ee687a0b72ec6f308eb9
tjmknam <tjmknam@gmail.com>
2016-09-12 12:00:07 -0500

    Create Constants.java

---
+++ directory/model/Constants.java
@@ -0,0 +1,10 @@
+public class Constants {
+	
+	public static final String USER_ID = "UserID";
+	public static final String PASSWORD = "password";
+	public static final String FIRST_NAME = "firstName";
+	public static final String LAST_NAME = "lastName";
+	public static final String EMAIL = "email";
+	public static final String USER_TYPE = "userType";
+
+}
