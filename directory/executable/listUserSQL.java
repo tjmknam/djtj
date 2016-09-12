fc7acd97d908b17ce02a0c2021ef2f777df83ac7
tjmknam <tjmknam@gmail.com>
2016-09-12 12:02:59 -0500

    Create listUserSQL.java

---
+++ directory/executable/listUserSQL.java
@@ -0,0 +1,12 @@
+import storage.*;
+
+public class listUserSQL {
+
+	public static void main(String[] args) {
+		DBConnectStorage connect = new DBConnectStorage();
+		connect.getData();
+		
+
+	}
+
+}
